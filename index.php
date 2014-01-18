ce9e281dffd85fb4433064973162613ca6dec5f9
whalther <walter.zetino@gmail.com>
2014-01-17 21:54:41 -0600

    cambio agredos

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat Multimedia</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link type="text/css" href="js/css/jquery-ui-1.9.2.custom.css" rel="stylesheet" />	
+        <link type="text/css" href="js/css/cssbody.css" rel="stylesheet" />	
+        <script  type="text/javascript" src="js/jquery-1.10.2.js"></script>
+        <script  type="text/javascript" src="js/jquery-ui-1.9.2.custom.js"></script> 
+   
+  <script>
+  $(function() {
+    $( "#draggable" ).draggable();
+  });
+  </script>
+</head>
+<body>
+ 
+<div id="draggable" class="ui-widget-content">
+    <span class="titulo">CHAT COMERCIAL</span><br><br>
+
+    <select name="select" style="height: 23px; color: #333333">
+  <option value="value1">DTH</option> 
+  <option value="value2" selected>HFC</option>
+  <option value="value3">Servicios Inalámbricos</option>
+</select>
+    <select name="select2" style="height: 23px; color: #333333">
+  <option value="value1">Solo básico</option> 
+  <option value="value2" selected>Doble básico</option>
+  <option value="value3">DTH - Doble avanzado</option>
+  <option value="value3">DTH - Triple básico 1 Mega</option>
+  <option value="value3">DTH - Triple básico 2 Mega</option>
+  <option value="value3">DTH - Triple básico 3 Mega</option>
+  <option value="value3">DTH - Triple básico 5 Mega</option>
+  <option value="value3">DTH - Triple avanzado 1 Mega</option>
+  <option value="value3">DTH - Triple avanzado 2 Mega</option>
+  <option value="value3">DTH - Triple avanzado 3 Mega</option>}
+  <option value="value3">DTH - Triple avanzado 5 Mega</option>
+  <option value="value3">DTH - Triple avanzado 1 Mega</option>
+  <option value="value3">DTH - Triple avanzado 2 Mega</option>
+  <option value="value3">DTH - Triple avanzado 3 Mega</option>}
+  <option value="value3">DTH - Triple avanzado 5 Mega</option>
+  <option value="value3">DTH - Triple avanzado 1 Mega</option>
+  <option value="value3">DTH - Triple avanzado 2 Mega</option>
+  <option value="value3">DTH - Triple avanzado 3 Mega</option>}
+  <option value="value3">DTH - Triple avanzado 5 Mega</option>
+ 
+ </select><br>
+ <textarea cols="46" rows="20" name="info" style="border: solid 1px #cccccc;">
+     
+ </textarea>
+ <span class="miboton">COPIAR</span>
+ <span class="miboton">MODIFICAR</span>
+ <span class="miboton">AGREGAR</span>
+ <span class="miboton">ELIMINAR</span>
+    
+</div>
+    </body>
+</html>
